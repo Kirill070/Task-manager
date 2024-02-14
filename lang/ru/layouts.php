bbf6c629a6964d94e45ce2ed3a551d7cf70df2ad
Kirill Chernyy <kirill070@gmail.com>
2024-02-14 15:40:04 +0300

    Rebuild Welcome

---
+++ lang/ru/layouts.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'app' => [
+        'app_name' => 'Менеджер задач',
+        'labels' => 'Метки',
+        'login' => 'Вход',
+        'logout' => 'Выход',
+        'register' => 'Регистрация',
+        'tasks' => 'Задачи',
+        'task_statuses' => 'Статусы'
+    ]
+];
