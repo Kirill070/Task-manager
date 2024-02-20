0894e3fc3af639ca26228cd4b28133576be55418
Kirill Chernyy <kirill070@gmail.com>
2024-02-20 15:49:11 +0300

    Make Home

---
+++ lang/ru/home.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'home' => [
+        'description' => 'Это простой менеджер задач на Laravel',
+        'header' => 'Привет от Хекслета!',
+        'learn_more' => 'Нажми меня'
+    ]
+];
